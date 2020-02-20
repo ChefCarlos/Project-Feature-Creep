2ac0e04057f42dc97628c0748d7b9a3717d6feb0
carl ind <mrnitrodown@gmail.com>
2020-02-20 20:55:02 +0000

    Initial Git Commit Currently you are able to move the ship in space

---
+++ Assets/Scripts/Ship Scripts/CameraController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    public float distance = 400.0f;
+    public Vector3 offsetFromTarget = new Vector3(0f, 0f, 0f);
+
+    ShipController shipController;
+
+    private float currentX = 0.0f;
+    private float currentY = 0.0f;
+    private float sensitivityX = 4.0f;
+    private float sensitivityy = 1.0f;
+
+    private const float Y_ANGLE_MIN = -50.0f;
+    private const float Y_ANGLE_MAX = 50.0f;
+
+
+    void Start()
+    {
+        SetCameraTarget(target);
+    }
+
+    public void SetCameraTarget(Transform t)
+    {
+        target = t;
+
+        if (target != null)
+        {
+            if (target.GetComponent<ShipController>())
+            {
+                shipController = target.GetComponent<ShipController>();
+            }
+            else
+                Debug.Log("Cameras target character doesn't have controller");
+        }
+        else
+            Debug.Log("Camera needs target");
+    }
+
+    private void Update()
+    {
+        currentX += Input.GetAxis("Mouse X");
+        currentY += Input.GetAxis("Mouse Y");
+        currentY = Mathf.Clamp(currentY, Y_ANGLE_MIN, Y_ANGLE_MAX);
+    }
+
+    void LateUpdate()
+    {
+        MoveToTarget();
+        LookAtTarget();
+        
+    }
+
+    void MoveToTarget()
+    {
+        transform.position = target.position + (target.rotation * offsetFromTarget);
+    }
+
+    void LookAtTarget()
+    {
+        if (Input.GetKey(KeyCode.Mouse1))
+        {
+            transform.position = target.position + (Quaternion.Euler(-currentY, currentX, 0) * new Vector3(0, 0, -distance));
+            transform.LookAt(target.position);
+        }else
+        transform.rotation = Quaternion.LookRotation(target.position - transform.position, target.up); 
+        
+    }
+}
---
+++ Assets/Scripts/Ship Scripts/ShipController.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+public class ShipController : MonoBehaviour
+{
+
+    public float accelerateSpeed = 1f;
+    public float maxForwardSpeed = 60f;
+    public float currentForwardSpeed = 0f;
+
+    public float turnSpeed = .1f;
+    public float maxTurnSpeed = 100f;
+    public float currentYawSpeed = 0f;
+    public float currentPitchSpeed = 0f;
+    public float currentRotateSpeed = 0f;
+
+   
+
+    Vector3 forwardVel;
+    Rigidbody rbody;
+    public float forwardInput, turnInput, rotateInput, tiltInput;
+
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody>();
+        forwardInput = turnInput = rotateInput = tiltInput = 0;
+    }
+
+    void GetInput()
+    {
+        forwardInput = Input.GetAxis("Vertical");
+        turnInput = Input.GetAxis("Horizontal");
+        rotateInput = Input.GetAxis("Rotate");
+        tiltInput = Input.GetAxis("Tilt");
+    }
+
+
+    void Update()
+    {
+        GetInput();
+        Rotate();
+    }
+
+    void FixedUpdate()
+    {
+        Accelerate();
+    }
+
+    void Accelerate()
+    {
+
+        if (currentForwardSpeed < maxForwardSpeed && forwardInput > 0)
+        {
+            currentForwardSpeed += accelerateSpeed * forwardInput;
+        }
+        else if (forwardInput == 0 && currentForwardSpeed != 0)
+        {
+            currentForwardSpeed = Mathf.SmoothStep(currentForwardSpeed, 0, accelerateSpeed / 10);
+        }
+        else if (currentForwardSpeed > -maxForwardSpeed && forwardInput < 0)
+        {
+            currentForwardSpeed += accelerateSpeed * forwardInput;
+        }
+
+        rbody.velocity = transform.forward * currentForwardSpeed;
+    }
+
+    void Rotate()
+    {
+        //Turn Sideways   
+        if (currentYawSpeed < maxTurnSpeed && turnInput > 0)
+        {
+            currentYawSpeed += turnSpeed * turnInput;
+        }
+        else if (turnInput == 0 && currentYawSpeed != 0)
+        {
+            currentYawSpeed = Mathf.SmoothStep(currentYawSpeed, 0, turnSpeed / 2);
+        }
+        else if (currentYawSpeed > -maxTurnSpeed && turnInput < 0)
+        {
+            currentYawSpeed += turnSpeed * turnInput;
+        }
+        //Tilt Forward/Back
+        if (currentPitchSpeed < maxTurnSpeed && tiltInput > 0)
+        {
+            currentPitchSpeed += turnSpeed * tiltInput;
+        }
+        else if (tiltInput == 0 && currentPitchSpeed != 0)
+        {
+            currentPitchSpeed = Mathf.SmoothStep(currentPitchSpeed, 0, turnSpeed / 2);
+        }
+        else if (currentPitchSpeed > -maxTurnSpeed && tiltInput < 0)
+        {
+            currentPitchSpeed += turnSpeed * tiltInput;
+        }
+        //Rotate Sideways
+        if (currentRotateSpeed < maxTurnSpeed && rotateInput > 0)
+        {
+            currentRotateSpeed += turnSpeed * rotateInput;
+        }
+        else if (rotateInput == 0 && currentRotateSpeed != 0)
+        {
+            currentRotateSpeed = Mathf.SmoothStep(currentRotateSpeed, 0, turnSpeed / 2);
+        }
+        else if (currentRotateSpeed > -maxTurnSpeed && rotateInput < 0)
+        {
+            currentRotateSpeed += turnSpeed * rotateInput;
+        }
+
+
+        float yaw = currentYawSpeed  * Time.deltaTime;
+        float pitch = currentPitchSpeed  * Time.deltaTime;
+        float rotate = currentRotateSpeed  * Time.deltaTime;
+
+        transform.Rotate(-pitch, yaw, rotate);
+    }
+
+
+    
+}
